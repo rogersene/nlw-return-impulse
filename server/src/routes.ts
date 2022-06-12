9944f5f76205a86c541ee3792d0d24f9193b6360
rogersene <rogersenefaria@gmail.com>
2022-06-12 17:16:36 -0300

    ajustes no backand para deploy

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,6 +8,7 @@ export const routes = express.Router();
 routes.post('/feedbacks', async (req,res) => {
   const { type, comment, screenshot } = req.body
 
+  try {
   const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
 
   const nodemailerMailAdapter = new NodemailerMailAdapter()
@@ -24,4 +25,9 @@ routes.post('/feedbacks', async (req,res) => {
   })
 
   return res.status(201).send()
+  } catch(err) {
+    console.error(err)
+    
+    return res.status(500).send
+  }
 })
